4138c7acc98bf6b9e46e046c16e7fe62e0eb16e2
Matthieu Ancellin <matthieu.ancellin@cmla.ens-cachan.fr>
2019-09-28 15:53:41 +0200

    Refactor maps.

--- labelled_functions/maps.py
+++ labelled_functions/maps.py
@@ -6,17 +6,21 @@ import pandas as pd
 
 from .labels import LabelledFunction
 
-def recorded_map(f, *args):
-    return map(LabelledFunction(f).recorded_call, *args)
-
-def pandas_map(f, *args):
-    f = LabelledFunction(f)
-    if len(args) == 1 and isinstance(args[0], pd.DataFrame):
-        return pandas_map(f, *[args[0][name] for name in f.input_names])
+def parse_input(input_names, *args, **kwargs):
+    if len(args) == 1 and len(kwargs) == 0 and isinstance(args[0], pd.DataFrame):
+        return [args[0][name] for name in input_names]
     else:
-        return pd.DataFrame(list(recorded_map(f, *args))).set_index(f.input_names)
+        return args + tuple(kwargs[name] for name in input_names[len(args):])
 
-def mapkw(f, *_, **kwargs):
+def lmap(f, *args, **kwargs):
     f = LabelledFunction(f)
-    return map(f, *[kwargs[name] for name in f.input_names])
+    return map(f, *parse_input(f.input_names, *args, **kwargs))
+
+def recorded_map(f, *args, **kwargs):
+    f = LabelledFunction(f)
+    return map(f.recorded_call, *parse_input(f.input_names, *args, **kwargs))
+
+def pandas_map(f, *args, **kwargs):
+    f = LabelledFunction(f)
+    return pd.DataFrame(list(recorded_map(f, *args, **kwargs))).set_index(f.input_names)
 
--- test/test_maps.py
+++ test/test_maps.py
@@ -3,6 +3,7 @@
 
 from hypothesis import given, settings
 from hypothesis.strategies import floats
+from hypothesis.extra.numpy import arrays
 
 import numpy as np
 import pandas as pd
@@ -12,47 +13,54 @@ from labelled_functions.maps import *
 from example_functions import *
 
 number = floats(min_value=-1e6, max_value=1e6, allow_nan=False, allow_infinity=False)
+array = arrays(dtype=np.float64, shape=(10,), elements=number)
+
+@given(array, array)
+@settings(max_examples=5)
+def test_map_with_several_kind_of_inputs(length, radius):
+    df = pd.DataFrame(data={'radius': radius, 'length': length})
+    assert (
+        list(map(cylinder_volume, radius, length))
+        == list(lmap(cylinder_volume, radius, length))
+        == list(lmap(cylinder_volume, length=length, radius=radius))
+        == list(lmap(cylinder_volume, df))
+    )
 
 def test_recorded_map():
     assert list(recorded_map(double, [1])) == [{'x': 1, 'double': 2}]
 
-@given(number, number, number, number)
-@settings(max_examples=10)
-def test_pandas_map(a, b, c, d):
+@given(array, array)
+@settings(max_examples=5)
+def test_pandas_map(a, b):
     assert np.all(
-        pandas_map(double, [a, b])
-        == pd.DataFrame(data={'x': [a, b], 'double': [2*a, 2*b]}).set_index('x')
+        pandas_map(double, a)
+        == pd.DataFrame(data={'x': a, 'double': 2*a}).set_index('x')
     )
     assert np.all(
-        pandas_map(sum, [a, b], [c, d])
-        == pd.DataFrame(data={'x': [a, b],
-                              'y': [c, d],
-                              'sum': [a+c, b+d],
+        pandas_map(sum, a, b)
+        == pd.DataFrame(data={'x': a,
+                              'y': b,
+                              'sum': a+b,
                               }
                         ).set_index(['x', 'y'])
     )
     assert np.all(
-        pandas_map(annotated_cube, [a, b])
-        == pd.DataFrame(data={'x': [a, b],
-                              'length': [12*a, 12*b],
-                              'area': [6*a**2, 6*b**2],
-                              'volume': [a**3, b**3],
+        pandas_map(annotated_cube, a)
+        == pd.DataFrame(data={'x': a,
+                              'length': 12*a,
+                              'area': 6*a**2,
+                              'volume': a**3,
                               }
                         ).set_index('x')
     )
 
 
-def test_pandas_map_on_df():
-    x, y = np.random.rand(10), np.random.rand(10)
+@given(array, array)
+@settings(max_examples=5)
+def test_pandas_map_on_df(x, y):
     df = pd.DataFrame(data={'x': x, 'y': y})
     assert np.all(
         pandas_map(sum, df).reset_index()
         == pd.DataFrame(data={'x': x, 'y': y, 'sum': x+y})
-    )
-
-def test_mapkw():
-    assert (
-        list(mapkw(cylinder_volume, radius=[4, 5, 6], length=[1, 2, 3]))
-        == list(mapkw(cylinder_volume, length=[1, 2, 3], radius=[4, 5, 6]))
     )
 
