7c444b89a074cd0a128e92535ce63d64be7a50c7
Matthieu Ancellin <matthieu.ancellin@cmla.ens-cachan.fr>
2019-10-29 18:05:09 +0100

    Update setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
-setup(name='labelled_functions',
-      version='0.1',
-      description='Python functions with metadata on their inputs and outputs',
-      author='Matthieu Ancellin',
-      author_email='matthie.ancellin@cmla.ens-cachan.fr',
-      license='MIT',
-      packages=['labelled_functions'],
-      )
+setup(
+    name='labelled_functions',
+    version='0.1',
+    description='Python functions with metadata on their inputs and outputs',
+    author='Matthieu Ancellin',
+    author_email='matthie.ancellin@cmla.ens-cachan.fr',
+    license='MIT',
+    install_requires=[
+        'pandas',
+        'xarray',
+        'parso',
+    ],
+    packages=['labelled_functions'],
+)
