7ace7b307420f9dcd4966c0ef2a5ea50fd4aae48
Matthieu Ancellin <matthieu.ancellin@cmla.ens-cachan.fr>
2019-10-29 16:26:56 +0100

    Imrpove implementation and testing of pipelines.

--- labelled_functions/compose.py
+++ labelled_functions/compose.py
@@ -1,10 +1,8 @@
 
 from .labels import Unknown, LabelledFunction
 
-def compose(funcs):
-    return pipeline(reversed(funcs))
 
-def pipeline(funcs, keep_all_internals=False, debug=False):
+def pipeline(funcs, *, keep_intermediate_outputs=False, debug=False, default_values=None):
     funcs = [LabelledFunction(f) for f in funcs]
 
     for f in funcs:
@@ -14,24 +12,41 @@ def pipeline(funcs, keep_all_internals=False, debug=False):
     all_inputs = [set(f.input_names) for f in funcs]
     all_outputs = [set(f.output_names) for f in funcs]
 
-    pipe_inputs, available = set(), set()
+    pipe_inputs, intermediate = set(), set()
     for f_inputs, f_outputs in zip(all_inputs, all_outputs):
-        pipe_inputs = pipe_inputs.union(f_inputs - available)
-        available = available.union(f_outputs)
 
-    pipe_outputs = available.copy()
-    if not keep_all_internals:
+        # The inputs that are not already in the set of intermediate variables
+        # are inputs of the whole pipeline
+        pipe_inputs = pipe_inputs.union(f_inputs - intermediate)
+
+        intermediate = intermediate.union(f_outputs)
+
+    pipe_outputs = intermediate.copy()
+    if not keep_intermediate_outputs:
         for f_inputs in all_inputs:
             pipe_outputs = pipe_outputs - f_inputs
 
     @LabelledFunction
     def pipe(**namespace):
+        if default_values is not None:
+            namespace = {**default_values, **namespace}
+
+        superfluous_inputs = set(namespace.keys()) - pipe_inputs
+        if len(superfluous_inputs) > 0:
+            raise TypeError(f"Pipeline got unexpected argument(s): {superfluous_inputs}")
+
         for f in funcs:
             if debug:
                 print(namespace.keys())
             namespace = f.apply_in_namespace(namespace)
+
         return {name: val for name, val in namespace.items() if name in pipe_outputs}
 
     pipe.input_names = list(pipe_inputs)
     pipe.output_names = list(pipe_outputs)
     return pipe
+
+
+def compose(funcs, **kwargs):
+    return pipeline(reversed(funcs, **kwargs))
+
--- test/test_compose.py
+++ test/test_compose.py
@@ -1,7 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pytest
 
 from labelled_functions.compose import compose, pipeline
 
 from example_functions import *
+
+
+def test_pipeline():
+    pipe = pipeline([random_radius, cylinder_volume])
+    assert pipe.input_names == ['length']
+    assert pipe.output_names == ['volume']
+    assert 0.0 < pipe(length=1.0)['volume'] < 2*compute_pi()
+
+    with pytest.raises(TypeError):
+        pipe(length=1.0, potato=1.0)
+
+
+def test_keep_intermediate_outputs():
+    # Only outputs
+    pipe = pipeline([random_radius, cylinder_volume], keep_intermediate_outputs=False)
+    result = pipe(length=1.0)
+    assert 'length' not in result.keys()
+    assert 'radius' not in result.keys()
+
+    # Outputs and intermediate variable
+    pipe = pipeline([random_radius, cylinder_volume], keep_intermediate_outputs=True)
+    result = pipe(length=1.0)
+    assert 'length' not in result.keys()
+    assert 'radius' in result.keys()
+
+    # Inputs and outputs but no intermediate variable
+    pipe = pipeline([random_radius, cylinder_volume], keep_intermediate_outputs=False)
+    result = pipe.recorded_call(length=1.0)
+    assert 'length' in result.keys()
+    assert 'radius' not in result.keys()
+
+    # Inputs and outputs and intermediate variable
+    pipe = pipeline([random_radius, cylinder_volume], keep_intermediate_outputs=True)
+    result = pipe.recorded_call(length=1.0)
+    assert 'length' in result.keys()
+    assert 'radius' in result.keys()
+
+
+def test_default_variables():
+    def f(a) -> 'b':
+        return 2*a
+    def g(b, c=3) -> 'output':
+        return c - b
+
+    pipe = pipeline([f, g])
+    assert pipe(a=1) == {'output': 1}
+    assert pipe(a=1, c=0) == {'output': -2}
+
+    pipe = pipeline([cube], default_values={'x': 1.0})
+    assert pipe()['volume'] == 1.0
+    assert pipe(x=10.0)['volume'] == 1000.0
 
 
 def test_compose():
@@ -10,8 +65,3 @@ def test_compose():
     assert composed.output_names == ['volume']
 
 
-def test_pipeline():
-    pipe = pipeline([random_radius, cylinder_volume])
-    assert pipe.input_names == ['length']
-    assert pipe.output_names == ['volume']
-    assert 0.0 < pipe(length=1.0)['volume'] < 2*compute_pi()
