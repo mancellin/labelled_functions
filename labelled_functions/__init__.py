5bc2f7a48b09cdecbc43e1834ba15117d5cc7f4c
Matthieu Ancellin <matthieu.ancellin@cmla.ens-cachan.fr>
2019-12-04 11:31:57 +0100

    Add time decorator.

--- labelled_functions/__init__.py
+++ labelled_functions/__init__.py
@@ -7,3 +7,4 @@ from .labels import label
 from .pipeline import pipeline, compose, let, show, relabel
 from .maps import pandas_map
 from .cartesian_products import full_parametric_study
+from .decorators import time
---
+++ labelled_functions/decorators.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""Some higher-order functions that make useful tools."""
+
+from labelled_functions.labels import label
+
+
+# API
+
+def time(func):
+    func = label(func)
+
+    def timed_func(*args, **kwargs):
+        from time import perf_counter
+        start = perf_counter()
+        result = func._output_as_dict(func(*args, **kwargs))
+        end = perf_counter()
+        result[f"{func.name}_execution_time"] = end - start
+        return result
+
+    timed_f = label(
+        timed_func,
+        _input_names=func._input_names,
+        output_names=func.output_names + [f"{func.name}_execution_time"],
+        default_values=func.default_values,
+        hidden_inputs=func.hidden_inputs,
+    )
+
+    return timed_f
